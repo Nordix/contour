0d8a656b533e02ba7e7e3aac02a5742236889cba
Tero Saarni <tero.saarni@est.tech>
2020-12-09 17:31:15 +0200

    internal/dag: trigger rebuild for configured secrets
    This change adds secret references from the configuration file to the list of
    secrets that will trigger DAG rebuild.  Previously only secrets referred by
    HTTPProxy and Ingress resources were considered.  The result was that secrets
    were not picked up correctly if they were created after the creation of
    HTTPProxy or Ingress themselves triggered a rebuild.
    
    Signed-off-by: Tero Saarni <tero.saarni@est.tech>

--- cmd/contour/serve.go
+++ cmd/contour/serve.go
@@ -55,6 +55,7 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/tools/cache"
 )
@@ -311,6 +312,14 @@ func doServe(log logrus.FieldLogger, ctx *serveContext) error {
 		}
 	}
 
+	var configuredSecretRefs []*types.NamespacedName
+	if fallbackCert != nil {
+		configuredSecretRefs = append(configuredSecretRefs, fallbackCert)
+	}
+	if clientCert != nil {
+		configuredSecretRefs = append(configuredSecretRefs, clientCert)
+	}
+
 	// Set up Prometheus registry and register base metrics.
 	registry := prometheus.NewRegistry()
 	registry.MustRegister(prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}))
@@ -401,9 +410,10 @@ func doServe(log logrus.FieldLogger, ctx *serveContext) error {
 		Observer:        dag.ComposeObservers(append(xdscache.ObserversOf(resources), snapshotHandler)...),
 		Builder: dag.Builder{
 			Source: dag.KubernetesCache{
-				RootNamespaces: ctx.proxyRootNamespaces(),
-				IngressClass:   ctx.ingressClass,
-				FieldLogger:    log.WithField("context", "KubernetesCache"),
+				RootNamespaces:       ctx.proxyRootNamespaces(),
+				IngressClass:         ctx.ingressClass,
+				ConfiguredSecretRefs: configuredSecretRefs,
+				FieldLogger:          log.WithField("context", "KubernetesCache"),
 			},
 			Processors: []dag.Processor{
 				&dag.IngressProcessor{
